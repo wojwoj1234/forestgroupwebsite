f8de75ae35f5f14458840c21c6d7ccd7579f54e9
Wojciech Gil <wojciechgilpriv@gmail.com>
2024-12-08 03:06:31 +0100

    Implement mobile navigation and enhance styles
    - Added JavaScript functionality for mobile navigation toggle.
    - Updated SCSS to include animations for mobile navigation.
    - Enhanced HTML structure for both desktop and mobile navigation.
    - Updated variables for consistent color usage.

---
+++ src/js/script.js
@@ -0,0 +1,32 @@
+const hamburgerBtn = document.querySelector('.hamburger-btn')
+const hamburgerIcon = document.querySelector('.hamburger-btn__icon')
+const mobileNav = document.querySelector('.nav-mobile')
+const mobileNavLinks = document.querySelectorAll('.nav-mobile__menu-link')
+
+const handleMobileNav = () => {
+	if (mobileNav.classList.contains('nav-mobile--active')) {
+		mobileNav.classList.add('nav-mobile--inactive')
+		hamburgerIcon.setAttribute('src', './dist/img/icons/menu.svg')
+		setTimeout(() => {
+			mobileNav.classList.remove('nav-mobile--active')
+		}, 700)
+	} else {
+		mobileNav.classList.toggle('nav-mobile--active')
+		mobileNav.classList.remove('nav-mobile--inactive')
+		hamburgerIcon.setAttribute(
+			'src',
+			mobileNav.classList.contains('nav-mobile--active') ? './dist/img/icons/x.svg' : './dist/img/icons/menu.svg'
+		)
+		mobileNavLinks.forEach(link => {
+			link.addEventListener('click', () => {
+				mobileNav.classList.add('nav-mobile--inactive')
+				hamburgerIcon.setAttribute('src', './dist/img/icons/menu.svg')
+				setTimeout(() => {
+					mobileNav.classList.remove('nav-mobile--active')
+				}, 700)
+			})
+		})
+	}
+}
+
+hamburgerBtn.addEventListener('click', handleMobileNav)
